13e11c3888455d72f2d11a68ffcb2b8f4e54e669
MarceloTizo <106279700+MarceloTizo@users.noreply.github.com>
2023-10-17 16:45:19 -0300

    insercao dos textos na pagina 2

--- projeto_tela_flutter/lib/paginaSecundaria.dart
+++ projeto_tela_flutter/lib/paginaSecundaria.dart
@@ -31,7 +31,7 @@ class PaginaSecundaria extends StatelessWidget {
             Container(
               width: 357,
               height: 200,
-              transform: Matrix4.translationValues(0, -196, 0),
+              transform: Matrix4.translationValues(0, -57, 0),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(17),
                 child: Image.asset(
@@ -40,18 +40,70 @@ class PaginaSecundaria extends StatelessWidget {
                 ),
               ),
             ),
-            Positioned(
-             
+            Align(
+              alignment: Alignment.centerLeft,
               child: Container(
-                padding: const EdgeInsets.all(8),
-                             transform: Matrix4.translationValues(0, -190, 0),
-
+                transform: Matrix4.translationValues(0, -50, 0),
                 child: const Text(
-                  'octuber 4, 2021 * 3 min read',
+                  '               octuber 4, 2021 * 3 min read',
                   style: TextStyle(
-                    
-                    fontSize: 14,
+                    fontSize: 11,
                     fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 15, 15, 15),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10), // Espaço entre os textos
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                transform: Matrix4.translationValues(18, -46, 0),
+                child: const Text(
+                  '  How to get started as a mobile app designer and get your first client?',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 15, 15, 15),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 22), // Espaço entre os textos
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                transform: Matrix4.translationValues(6, -29, 0),
+                child: const Text(
+                  '  Everyone wants to make the next great mobile app. It can be an extremely profitable way to make some money if you know what youre doing.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color.fromARGB(255, 15, 15, 15),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                transform: Matrix4.translationValues(3, -19, 0),
+                child: const Text(
+                  '  If you ve got a great mobile app idea and decided to consult with a developer or an app development company, you may have been surprised to hear how costly it is to outsource development.',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color.fromARGB(255, 15, 15, 15),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                transform: Matrix4.translationValues(6, -1, 0),
+                child: const Text(
+                  '  So thats when the thought hit you, I can just do learn to do this myself."',
+                  style: TextStyle(
+                    fontSize: 14,
                     color: Color.fromARGB(255, 15, 15, 15),
                   ),
                 ),
@@ -60,11 +112,22 @@ class PaginaSecundaria extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: TextButton(
+          onPressed: () {
+            // Ação a ser executada quando o botão "Read more" for clicado
+          },
+          child: const Text(
+            'Read more',
+            style: TextStyle(
+              
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
-
-
-
-
